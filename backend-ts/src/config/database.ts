0ff6dd6f4c8cdc6647ce37daf808ed40a8112a76
MandoV0 <burakgulduz@gmail.com>
2025-09-06 14:47:42 +0200

    Miau

--- backend-ts/src/config/database.ts
+++ backend-ts/src/config/database.ts
@@ -6,7 +6,7 @@ dotenv.config();
 export const pool = new Pool({
   user: process.env.DB_USER || "postgres",
   password: process.env.DB_PASSWORD || "gamebacklog",
-  database: process.env.DB_NAME || "game_backlog",
+  database: process.env.DB_NAME || "game_backlog_v1",
   host: process.env.DB_HOST || "localhost",
   port: Number(process.env.DB_PORT) || 5432,
 });
