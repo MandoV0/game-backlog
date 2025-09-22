22e5205907bbc37b98e4627a262f61fbf37301fd
MandoV0 <burakgulduz@gmail.com>
2025-09-22 21:01:39 +0200

    Trying to configure database config in backend to connect to render psql.

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,3 +1,4 @@
+/*
 import { Pool } from "pg";
 import dotenv from "dotenv";
 
@@ -14,3 +15,18 @@ export const pool = new Pool({
 pool.connect()
     .then(() => console.log("Connected to PostgreSQL"))
     .catch((err: Error) => console.error("PostgreSQL connection error:", err));
+*/
+
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+pool.connect()
+  .then(() => console.log("Connected to PostgreSQL"))
+  .catch((err: Error) => console.error("PostgreSQL connection error:", err));
